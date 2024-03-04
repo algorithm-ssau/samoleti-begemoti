b89ff2e990635845067d8f8d47cf8e17d4ddc72f
kazkate <KazakovaEkater@yandex.ru>
2024-03-04 12:00:27 +0400

    список отелей

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,3 +1,4 @@
+
 interface CardProps {
     title: string,
     description?: string,
@@ -10,4 +11,65 @@ export function Card(props: CardProps) {
         <h3>{props.title}</h3>
         <p>{ props.description} </p>
     </div>)
+}
+interface CardHotels {
+    title: string,
+    description?: string,
+    id: string,
+    city: CITIES,
+    price: number,
+    adress: string,
+    peopleInRoom: string[],
+    onHotelClicked: (id: string) => void,
+}
+export enum CITIES{
+    SAMARA = "Самара",
+    MOSCOW = "Москва",
+    SAINT_PETERBSBURG = "Санкт-Петербург",
+    VLADIVOSTOK = "Владивосток",
+    SAKHALIN ="Сахалин"
+}
+export let hotels:CardHotels[]= [
+    {title :"Первый", id :"Первый", city :CITIES.MOSCOW, price : 3200, adress: 'Планета Земля', peopleInRoom : ['2- ', '3- '], onHotelClicked: (text) => alert(text) },
+    {title :"Второй", id :"Второй", city :CITIES.SAMARA, price : 1200, adress: 'Планета Земля', peopleInRoom : ['2- ', '3- ', '4- '], onHotelClicked: (text) => alert(text)},
+    {title :"Третий", id :"Третий", city :CITIES.SAINT_PETERBSBURG, price : 2200, adress: 'Планета Земля', peopleInRoom : ['1- '], onHotelClicked: (text) => alert(text) },
+    {title :"Четвертый", id :"Четвертый", city :CITIES.MOSCOW, price : 4200, adress: 'Планета Земля', peopleInRoom : ['1- ', '2- ', '3- '], onHotelClicked: (text) => alert(text) },
+    {title :"Пятый", id :"Пятый", city :CITIES.SAMARA, price : 3200, adress: 'Планета Земля', peopleInRoom : ['2- ', '3- '], onHotelClicked: (text) => alert(text)},
+    {title :"Шестой", id :"Шестой", city :CITIES.MOSCOW, price : 1000, adress: 'Планета Земля', peopleInRoom : ['1- ', '2- ', '3- ', '4- '], onHotelClicked: (text) => alert(text) },
+    {title :"Седьмой", id :"Седьмой", city :CITIES.MOSCOW, price : 5200, adress: 'Планета Земля', peopleInRoom : ['1- ', '2- '], onHotelClicked: (text) => alert(text) }
+]
+export function GetHotels(cityprop: CITIES): CardHotels[]{
+    let res_hotels: CardHotels[] = [];
+    for (let i = 0; i< hotels.length; i++){
+        if(cityprop===hotels[i]!.city){
+            res_hotels.push(hotels[i]!);
+        }
+    }
+    return res_hotels;
+}
+export function CardH(props: CardHotels) {
+    return (<div className="cardHotel" onClick={() => props.onHotelClicked(props.id)}>
+        <img className="img"
+                src='home.jpg' 
+                alt="Домик"                
+        />
+        <div className="info">
+            <h3>{props.title}</h3>
+            <p>Город: { props.city} </p>
+            <p>Адрес: {props.adress}</p>
+            <p>Минимальная цена за ночь: {props.price}</p>
+            <p>Условия: {props.peopleInRoom} местные номера</p>
+        </div>
+    </div>)
+}
+
+export function PrintHotels(cityprop: CITIES){
+    let hotels = GetHotels(cityprop);
+    let res_hotels = [];
+    for (let i=0; i < hotels.length; i++){
+        res_hotels.push(CardH(hotels[i]!));
+        } 
+    return <div className="containerHotels">
+        {res_hotels}
+    </div> ;
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,19 +1,32 @@
 import { createRoot } from "react-dom/client";
 import { test } from "./data";
-import { Card } from "./components/Card";
+//import { Card } from "./components/Card";
+
+import { CITIES } from "./components/Card";
+//import { CardH } from "./components/Card";
+import { PrintHotels } from "./components/Card";
+import './index.css';
 
 function App() {
     let x: number = 1;
     console.log(test())
     console.log(x);
-    return <div className="color">
-        <Card 
-           title="Да не умер"
-           id="да не умер он"
-           description="он в конце драйва" 
-           onClick={ (text) => alert(text) }
-        />
-    </div>
+    //let print = PrintHotels(CITIES.MOSCOW);
+
+    // return (<div className="color">
+    //     <Card 
+    //        title="Да не умер"
+    //        id="да не умер он"
+    //        description="он в конце драйва" 
+    //        onClick={ (text) => alert(text) }
+    //     />
+    // </div>
+    // {print}
+     return <div>
+        {PrintHotels(CITIES.MOSCOW)}
+
+     </div>;
+    
 }
 
 let container = document.getElementById("root");
