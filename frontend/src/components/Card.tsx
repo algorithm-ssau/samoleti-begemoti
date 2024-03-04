098a37b4936abc81ccd150f2dd79fe4fb39dc405
kazkate <KazakovaEkater@yandex.ru>
2024-03-04 16:46:10 +0400

    Список отелей

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,75 +1,63 @@
-
-interface CardProps {
+interface Hotel{
     title: string,
     description?: string,
     id: string,
-    onClick: (id: string) => void,
-}
-
-export function Card(props: CardProps) {
-    return (<div onClick={() => props.onClick(props.id)}>
-        <h3>{props.title}</h3>
-        <p>{ props.description} </p>
-    </div>)
-}
-interface CardHotels {
-    title: string,
-    description?: string,
-    id: string,
-    city: CITIES,
+    city: City,
     price: number,
-    adress: string,
+    address: string,
     peopleInRoom: string[],
+}
+
+interface HotelCardProps {
+    hotel: Hotel,
     onHotelClicked: (id: string) => void,
 }
-export enum CITIES{
-    SAMARA = "Самара",
-    MOSCOW = "Москва",
-    SAINT_PETERBSBURG = "Санкт-Петербург",
-    VLADIVOSTOK = "Владивосток",
-    SAKHALIN ="Сахалин"
+
+export enum City{
+    Samara = "Самара",
+    Moscow = "Москва",
+    SaintPetersburg = "Санкт-Петербург",
+    Vladivostok = "Владивосток",
+    Sakhalin ="Сахалин"
 }
-export let hotels:CardHotels[]= [
-    {title :"Первый", id :"Первый", city :CITIES.MOSCOW, price : 3200, adress: 'Планета Земля', peopleInRoom : ['2- ', '3- '], onHotelClicked: (text) => alert(text) },
-    {title :"Второй", id :"Второй", city :CITIES.SAMARA, price : 1200, adress: 'Планета Земля', peopleInRoom : ['2- ', '3- ', '4- '], onHotelClicked: (text) => alert(text)},
-    {title :"Третий", id :"Третий", city :CITIES.SAINT_PETERBSBURG, price : 2200, adress: 'Планета Земля', peopleInRoom : ['1- '], onHotelClicked: (text) => alert(text) },
-    {title :"Четвертый", id :"Четвертый", city :CITIES.MOSCOW, price : 4200, adress: 'Планета Земля', peopleInRoom : ['1- ', '2- ', '3- '], onHotelClicked: (text) => alert(text) },
-    {title :"Пятый", id :"Пятый", city :CITIES.SAMARA, price : 3200, adress: 'Планета Земля', peopleInRoom : ['2- ', '3- '], onHotelClicked: (text) => alert(text)},
-    {title :"Шестой", id :"Шестой", city :CITIES.MOSCOW, price : 1000, adress: 'Планета Земля', peopleInRoom : ['1- ', '2- ', '3- ', '4- '], onHotelClicked: (text) => alert(text) },
-    {title :"Седьмой", id :"Седьмой", city :CITIES.MOSCOW, price : 5200, adress: 'Планета Земля', peopleInRoom : ['1- ', '2- '], onHotelClicked: (text) => alert(text) }
-]
-export function GetHotels(cityprop: CITIES): CardHotels[]{
-    let res_hotels: CardHotels[] = [];
-    for (let i = 0; i< hotels.length; i++){
-        if(cityprop===hotels[i]!.city){
-            res_hotels.push(hotels[i]!);
-        }
-    }
-    return res_hotels;
+
+export let hotels : Hotel[]= [ 
+    {title :"Первый", id :"Первый", city :City.Moscow, price : 3200, address: 'Планета Земля', peopleInRoom : ['2- ', '3- ']},
+    {title :"Второй", id :"Второй", city :City.Samara, price : 1200, address: 'Планета Земля', peopleInRoom : ['2- ', '3- ', '4- ']},
+    {title :"Третий", id :"Третий", city :City.SaintPetersburg, price : 2200, address: 'Планета Земля', peopleInRoom : ['1- ']},
+    {title :"Четвертый", id :"Четвертый", city :City.Moscow, price : 4200, address: 'Планета Земля', peopleInRoom : ['1- ', '2- ', '3- ']},
+    {title :"Пятый", id :"Пятый", city :City.Samara, price : 3200, address: 'Планета Земля', peopleInRoom : ['2- ', '3- ']},
+    {title :"Шестой", id :"Шестой", city :City.Moscow, price : 1000, address: 'Планета Земля', peopleInRoom : ['1- ', '2- ', '3- ', '4- ']},
+    {title :"Седьмой", id :"Седьмой", city :City.Moscow, price : 5200, address: 'Планета Земля', peopleInRoom : ['1- ', '2- ']}
+];
+
+export function getHotels(city: City): Hotel[]{
+    let resHotels: Hotel[] = hotels.filter((aHotel)=>aHotel.city===city);
+    return resHotels;
 }
-export function CardH(props: CardHotels) {
-    return (<div className="cardHotel" onClick={() => props.onHotelClicked(props.id)}>
+
+export function HotelCard(props: HotelCardProps) {
+    return (<div className="hotelCard" onClick={() => props.onHotelClicked(props.hotel.id)}>
         <img className="img"
                 src='home.jpg' 
                 alt="Домик"                
         />
         <div className="info">
-            <h3>{props.title}</h3>
-            <p>Город: { props.city} </p>
-            <p>Адрес: {props.adress}</p>
-            <p>Минимальная цена за ночь: {props.price}</p>
-            <p>Условия: {props.peopleInRoom} местные номера</p>
+            <h3>{props.hotel.title}</h3>
+            <p>Город: { props.hotel.city} </p>
+            <p>Адрес: {props.hotel.address}</p>
+            <p>Минимальная цена за ночь: {props.hotel.price}</p>
+            <p>Условия: {props.hotel.peopleInRoom} местные номера</p>
         </div>
     </div>)
 }
-
-export function PrintHotels(cityprop: CITIES){
-    let hotels = GetHotels(cityprop);
-    let res_hotels = [];
-    for (let i=0; i < hotels.length; i++){
-        res_hotels.push(CardH(hotels[i]!));
-        } 
-    return <div className="containerHotels">
-        {res_hotels}
+interface HotelListProps{
+    city: City,
+}
+export function HotelList(prop: HotelListProps){
+    let hotels = getHotels(prop.city);
+    let resHotels = hotels.map((aHotel)=> <HotelCard hotel = {aHotel} onHotelClicked = {(text)=> alert(text)}/>);
+    return <div className="hotelsContainer">
+        {resHotels}
     </div> ;
 }
