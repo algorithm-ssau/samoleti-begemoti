c7e8bdec83cfe2504f0325d172be69f81a3fb0cb
varizonix <varizonix@gmail.com>
2024-03-06 13:25:24 +0400

    PlacePicker

---
+++ frontend/src/components/PlacePicker.tsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+
+export type DataType = {
+    place: string, 
+    date: string,
+    hours: string,
+    minutes: string,
+}
+
+type Props = {
+    listOfPlaces: string[],
+    onSubmit: (data: DataType) => void,
+}
+
+type SelectProps = {
+    selectList?: string[],
+    emptySelectSlot?: string,
+    onInput: (data: string) => void,
+}
+
+function DateSelect (props: SelectProps) {
+    return (
+        <input type='date' onChange={(e) => {props.onInput(e.target.value)}}></input>
+    )
+}
+
+function SmartSelect (props: SelectProps) {
+    const minSelect = props.selectList?.map(item => <option key={item}>{item}</option>)
+    return (
+        <select onChange={(e) => {props.onInput(e.target.value)}}>
+            <option value="" selected disabled hidden>{props.emptySelectSlot}</option>{minSelect}
+        </select>
+    )
+}
+
+export const PlacePicker = (props: Props) => {
+    const [place, setPlace] = useState("")
+    const [date, setDate] = useState("")
+    const [hours, setHours] = useState("")
+    const [minutes, setMinutes] = useState("")
+    const hoursList = ['00', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', 
+    '16', '17', '18', '19', '20', '21', '22', '23']
+    const minutesList = ['00','15', '30', '45']
+    return (
+        <div>
+            <text>Выбор места: </text>
+            <SmartSelect selectList={props.listOfPlaces} onInput={setPlace} emptySelectSlot="место"/>
+            <br/>
+            <text>Выбор времени: </text>
+            <DateSelect onInput={setDate}/>
+            <SmartSelect selectList={hoursList}  onInput={setHours} emptySelectSlot="--"/>
+            <SmartSelect selectList={minutesList}  onInput={setMinutes} emptySelectSlot="--"/>
+            <br/>
+            <button onClick={() => {
+                let data = {place, date, hours, minutes}
+                const isFull = Object.values(data).every(x => x !== null && x !== '')
+                if (isFull)
+                    props.onSubmit(data)
+                else alert('Заполните все поля')}}>Выбрать</button>
+        </div>
+    )
+}
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,20 @@
 import { createRoot } from "react-dom/client";
 import { City } from "./components/Card";
 import { HotelList } from "./components/Card";
+import { PlacePicker } from "./components/PlacePicker";
+import  type {DataType}  from "./components/PlacePicker";
 import './index.css';
-function App() {
 
-    
+function onSubmitHandle (data: DataType) {
+   console.log(data.place + ' ' + data.date + ' '+data.hours + ':' + data.minutes)
+}
+
+function App() {
+   const listOfPlaces = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
      return <div>
+
         < HotelList  city={City.Moscow} />;
+        <PlacePicker onSubmit = {onSubmitHandle} listOfPlaces= {listOfPlaces}/>
      </div>;
     
 }
