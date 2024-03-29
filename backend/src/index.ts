2976af647c5c07c90cf03c93cc0ec7a3a06eaf32
Golosoman <constan.lapin2015@mail.ru>
2024-03-29 12:13:02 +0400

    add some changes in index.ts and utils.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,29 @@
 import express from "express";
-import { test } from './util'
+import { test, get } from "./util";
+import { mongoURL, dbNameTest } from "./config";
+import MongoConnector from "./dbConnector";
+import * as Routers from "./routers/index";
 
 let app = express();
+app.use(express.json());
+MongoConnector.connect(mongoURL, dbNameTest);
 
-app.get('/', (req, res) => {
+app.get("/", async (req, res) => {
     res.send(test());
-})
+    const rese = await get("/hotels");
+    console.log(Object.keys(rese.data[0]));
+    console.log(rese.data[0]);
+});
+
+app.use("/", Routers.addressRouter);
+app.use("/", Routers.hotelRouter);
+app.use("/", Routers.hotelFacilityRouter);
+app.use("/", Routers.roomCategoryRouter);
+app.use("/", Routers.roomRouter);
+app.use("/", Routers.photoRouter);
 
 app.listen(3000, () => {
-    console.log("ехал гослинг навстречу концовке\n сценарист сказал не переживай\n гослинг ответил не переживу")
-})
+    console.log(
+        "ехал гослинг навстречу концовке\n сценарист сказал не переживай\n гослинг ответил не переживу"
+    );
+});
--- backend/src/util.ts
+++ backend/src/util.ts
@@ -1,3 +1,10 @@
+import axios from "axios";
+import { baseURL } from "./config";
+
+axios.defaults.baseURL = baseURL;
+
+export const get = async (url: string) => await axios.get(url);
+
 export function test() {
-    return "да не умер он в конце драйва"
+    return "да не умер он в конце драйва";
 }
