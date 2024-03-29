77bc19efd89e382cc3e99560009fed3d3a12b859
Golosoman <constan.lapin2015@mail.ru>
2024-03-29 12:12:02 +0400

    add config

---
+++ backend/src/config.ts
@@ -0,0 +1,4 @@
+export let baseURL: string = "http://localhost:3000";
+export let mongoURL: string = "mongodb://127.0.0.1:27017/";
+export let dbName: string = "SamoletiBegemoti";
+export let dbNameTest: string = "tests";
