e270c2d78c54f12ac56d5c3f1b69b7f24fb6501a
Salavat Nursafin <shattered946@gmail.com>
2024-04-23 09:51:14 +0400

    fix common module

--- common/src/db_types.ts
+++ common/src/db_types.ts
@@ -1,18 +1,18 @@
-import { Review, User } from "./user_type"
+import { Review, User } from "./user_type";
 
 export interface Hotel {
-    name: string,
-    description: string,
-    photos: Photo[]
-    address: Address,
-    rooms: Room[],
-    reviews: Review[]
+    name: string;
+    description: string;
+    photos: Photo[];
+    address: Address;
+    rooms: Room[];
+    reviews: Review[];
 }
 
-export interface Address  {
-    city: string,
-    country: string,
-    place: string,
+export interface Address {
+    city: string;
+    country: string;
+    place: string;
 }
 
 enum BookingStatus {
@@ -23,44 +23,44 @@ enum BookingStatus {
 }
 
 export interface HotelBooking {
-    room: Room,
-    dateStart: Date,
-    dateEnd: Date,
-    status: BookingStatus,
+    room: Room;
+    dateStart: Date;
+    dateEnd: Date;
+    status: BookingStatus;
 }
 
 export interface HotelTransaction {
-    type: "hotel",
-    room: Room,
+    type: "hotel";
+    room: Room;
 }
 
 export interface AirplaneTransaction {
-    type: "airplane",
-    airplaneInfo: string,
+    type: "airplane";
+    airplaneInfo: string;
 }
 
-export type SomeTransaction = HotelTransaction | AirplaneTransaction
+export type SomeTransaction = HotelTransaction | AirplaneTransaction;
 
 enum RoomCategory {
     Luxary,
     Normal,
-    Shit
+    Shit,
 }
 
 export interface HotelFacilities {
-    name: string,
+    name: string;
 }
 
 export interface Room {
-    category: RoomCategory,
-    price: number,
-    bedAmount: number,
-    facilities: HotelFacilities[],
-    number: number,
+    category: RoomCategory;
+    price: number;
+    bedAmount: number;
+    facilities: HotelFacilities[];
+    number: number;
 }
 
 export interface Photo {
-    url: string,
+    url: string;
 }
 
 export const x = 124;
