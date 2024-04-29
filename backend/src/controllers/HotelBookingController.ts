8c0534f4347f8adaedc94ff69921ffb00d1a96fe
Mort3gar <2021-01422@students.ssau.ru>
2024-04-29 19:09:45 +0400

    Изменения в Generic контроллере, добавлена схема HotelBooking (нужна для User), контроллеры User и HotelBooking

---
+++ backend/src/controllers/HotelBookingController.ts
@@ -0,0 +1,42 @@
+import HotelBookingModel from "../models/HotelBooking";
+import { HotelBooking } from "../types/db_types";
+import { GenericController } from "./GenericController";
+
+export class HotelBookingController extends GenericController<
+    HotelBooking,
+    typeof HotelBookingModel
+> {
+    constructor() {
+        super(HotelBookingModel);
+    }
+
+    createErrorMessages(): [string] {
+        return ["Не удалось создать пользователя"];
+    }
+
+    getAllErrorMessages(): [string] {
+        return ["Не удалось получить пользователя"];
+    }
+
+    getOneErrorMessages(): [string, string, string] {
+        return [
+            "Пользователь не найден",
+            "Не удалось вернуть пользователя",
+            "Не удалось получить пользователя",
+        ];
+    }
+    deleteOneErrorMessages(): [string, string, string] {
+        return [
+            "Пользователь не найден",
+            "Не удалось вернуть пользователя",
+            "Не удалось получить пользователя",
+        ];
+    }
+    updateOneErrorMessages(): [string, string, string] {
+        return [
+            "Пользователь не найден",
+            "Не удалось вернуть пользователя",
+            "Не удалось получить пользователя",
+        ];
+    }
+}
