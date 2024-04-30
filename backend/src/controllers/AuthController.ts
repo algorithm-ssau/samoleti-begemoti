873cf538fab02efb095c0e55a792ec482a725beb
Mort3gar <2021-01422@students.ssau.ru>
2024-04-30 20:51:22 +0400

    Auth логин

--- backend/src/controllers/AuthController.ts
+++ backend/src/controllers/AuthController.ts
@@ -39,4 +39,42 @@ export class AuthController{
             });
         }
     }
+
+    async login(req: Request, res: Response){
+        try {
+            const email = req.body.login;
+            const password = req.body.password;
+            let user = await UserController.getOneByKey("email", email);
+            if(!user || !bcrypt.compareSync(password, user.passwordHash)){
+                return res.status(401).json({
+                    message: "Неверный логин и/или пароль"
+                });
+            }
+            res.status(200).json({
+                token: jwt.sign({
+                    user_id: (user as User)._id,
+                    email: (user as User).email
+                },
+                jwt_access,
+                {expiresIn: ACCESS_EXPIRES_IN}
+                )
+            });
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({
+                message: "Что-то пошло не так..."
+            });
+        }
+    }
+
+    async logout(req: Request, res: Response){
+        try {
+                res.status(200).send();
+        } catch (error){
+            console.log(error)
+            res.status(500).json({
+                message: "Что-то пошло не так..."
+            });
+        }
+    }
 }
--- backend/src/routers/AuthRouter.ts
+++ backend/src/routers/AuthRouter.ts
@@ -4,4 +4,5 @@ import { AuthController } from "../controllers/index";
 export const authRouter = express.Router();
 
 authRouter
-    .post("/auth/register", AuthController.register);
+    .post("/auth/register", AuthController.register)
+    .post("/auth/login", AuthController.login);
