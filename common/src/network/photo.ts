1aada0dbf304a5bcbb4900bf1e7ef1c6552a7921
Golosmanchick <constan.lapin2015@mail.ru>
2024-05-14 14:13:01 +0400

    Added axios queries for photo

---
+++ common/src/network/photo.ts
@@ -0,0 +1,101 @@
+import axios, { Axios, AxiosInstance, CreateAxiosDefaults } from "axios";
+import { Photo, AuthSuccess } from "../db_types";
+
+// GET /photos
+// GET /photos/:id
+// POST /photos
+// DELETE /photos/:id
+// PATCH /photos/:id
+
+export class Network {
+    private axios: AxiosInstance;
+    constructor(
+        private getInstance: (config: CreateAxiosDefaults) => AxiosInstance
+    ) {
+        this.axios = getInstance({});
+    }
+
+    setToken(token: string) {
+        this.axios = this.getInstance({
+            headers: {
+                Authorization: token,
+            },
+        });
+    }
+
+    allPhotos() {
+        return getAllPhotos();
+    }
+
+    photoById(id: number) {
+        return getOnePhotoById(id);
+    }
+
+    create(photo: Photo) {
+        return createPhoto(this.axios, photo);
+    }
+
+    deleteById(id: number) {
+        return deletePhotoById(this.axios, id);
+    }
+
+    updateById(id: number) {
+        return updatePhotoById(this.axios, id);
+    }
+}
+
+/**
+ * Possible errors:
+ *
+ * status 500 - internal server error
+ *
+ */
+export async function getAllPhotos() {
+    return axios.get(`/photos`);
+}
+
+/**
+ * Possible errors:
+ *
+ * status 404 - hotel not found
+ *
+ * status 500 - internal server error
+ *
+ */
+export async function getOnePhotoById(id: number) {
+    return axios.get(`/photos/${id}`);
+}
+
+/**
+ * Possible errors:
+ *
+ * status 500 - internal server error
+ *
+ */
+export async function createPhoto(axios: AxiosInstance, photo: Photo) {
+    return axios.post<AuthSuccess>(`/photos`, photo);
+}
+
+/**
+ * Possible errors:
+ *
+ * status 404 - hotel not found
+ *
+ * status 500 - internal server error
+ *
+ */
+export async function deletePhotoById(axios: AxiosInstance, id: number) {
+    return axios.delete<AuthSuccess>(`/photos/${id}`);
+}
+
+/**
+ * Possible errors:
+ *
+ * status 404 - hotel not found
+ *
+ * status 500 - internal server error
+ *
+ */
+export async function updatePhotoById(axios: AxiosInstance, id: number) {
+    return axios.patch<AuthSuccess>(`/photos/${id}`);
+}
