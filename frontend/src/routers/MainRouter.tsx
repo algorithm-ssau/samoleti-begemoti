540eada43bef39746e6c883f0c3ccba083bd7cda
kazkate <KazakovaEkater@yandex.ru>
2024-05-19 13:11:07 +0400

    merge commit

--- frontend/src/routers/MainRouter.tsx
+++ frontend/src/routers/MainRouter.tsx
@@ -1,21 +1,32 @@
-import { Routes, Route } from "react-router";
+import { Route, Outlet } from "react-router";
 import { MainPage } from "../pages/MainPage";
 import { HotelPage } from "../pages/HotelPage";
 import { TestPage } from "../pages/TestPage";
 import { ProfilePage } from "../pages/ProfilePage";
 import { AuthAndEntryPage } from "../pages/AuthAndEntryPage";
+import { DefaultHeader } from "../components/Header/DefaultHeader";
+export function Layout() {
+    return (
+        <>
+            <DefaultHeader />
+            <Outlet />
+        </>
+    );
+}
 
 export function MainRouter() {
     return (
-        <Routes>
-            <Route path="/" element={<MainPage />} />
-            <Route path="/auth/*" element={<AuthAndEntryPage />} />
-            <Route path="hotel" element={<HotelPage id={0} />} />
-            <Route path="test" element={<TestPage />} />
-            <Route
-                path="profile/*"
-                element={<ProfilePage log={true} />}
-            ></Route>
-        </Routes>
+        <>
+            <Route element={<Layout />}>
+                <Route path="/" element={<MainPage />} />
+                <Route path="/auth/*" element={<AuthAndEntryPage />} />
+                <Route path="hotel" element={<HotelPage id={0} />} />
+                <Route path="test" element={<TestPage />} />
+                <Route
+                    path="profile/*"
+                    element={<ProfilePage log={true} />}
+                ></Route>
+            </Route>
+        </>
     );
 }
