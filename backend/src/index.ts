80d0fbf6b137cd0203f4c1c6efc9360f35984046
Salavat Nursafin <shattered946@gmail.com>
2024-05-31 13:42:12 +0400

    split app setup and launch

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,43 +1,8 @@
-import express, { Router } from "express";
-import { test } from "./util";
+import { app } from "./app";
 import { mongoURL, dbNameTest, port } from "./config";
 import MongoConnector from "./dbConnector";
-import * as Routers from "./routers/index";
-import { authMiddleware } from "./middleware/AuthMiddleware";
-import { errorMiddleware } from "./middleware/ErrorLoggerMiddleware";
-
-export const app = express();
-app.use(express.json());
 
 MongoConnector.connect(mongoURL, dbNameTest);
-
-app.use((req, res, next) => {
-    console.log(`got request with url: ${req.url}`);
-    next();
-});
-
-app.use("/", Routers.addressRouter);
-app.use("/", Routers.hotelRouter);
-app.use("/", Routers.hotelFacilityRouter);
-app.use("/", Routers.roomCategoryRouter);
-app.use("/", Routers.roomRouter);
-app.use("/", Routers.photoRouter);
-
-app.use("/", Routers.userRouter);
-app.use("/", Routers.authRouter);
-app.use("/", Routers.hotelBookingRouter);
-
-app.use(
-    Router()
-        .use(authMiddleware)
-        .get("/", async (req, res) => {
-            console.log(`received claim: ${JSON.stringify(req.claim)}`);
-            res.send(test());
-        })
-);
-
-app.use(errorMiddleware);
-
 app.listen(port, () => {
     console.log(
         "ехал гослинг навстречу концовке\n сценарист сказал не переживай\n гослинг ответил не переживу"
