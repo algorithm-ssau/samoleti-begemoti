64c8e5af1f4219eee836c2c36d0d1b80312f1c7e
Golosmanchick <constan.lapin2015@mail.ru>
2024-06-01 23:26:46 +0400

    Добавлены модельки

---
+++ backend/src/models/BankAccount.ts
@@ -0,0 +1,19 @@
+import { Schema, model } from "mongoose";
+import { BankAccount as BankAcc } from "../types/user_type";
+
+// export interface BankAccount {
+//     amount: BigInt;
+//     transactions: AccountTransaction[];
+// }
+
+const BankAccount = new Schema<BankAcc>(
+    {
+        amount: { type: BigInt, required: true },
+        transactions: [{ type: Schema.Types.ObjectId, ref: "AccountTransaction", required: true }]
+    },
+    {
+        timestamps: true,
+    }
+);
+
+export default model("BankAccount", BankAccount);
