a4a0a2e8ba1176542df4643768d49d4911641080
varizonix <varizonix@gmail.com>
2024-06-02 18:22:50 +0400

    Поиск отелей получше + отели в отдельном файле

--- frontend/src/components/SearchHotel.tsx
+++ frontend/src/components/SearchHotel.tsx
@@ -16,6 +16,11 @@ import HotelCard from "./HotelCard";
 import type { Address } from "samolet-common";
 import type { THotel } from "samolet-common/src/network/hotel";
 import { Photo } from "./HotelReviewComponent/style";
+import {
+    hotelInfo,
+    type Filters,
+    type HotelInfoProps,
+} from "../example_data/hotelInfo";
 
 const Container = styled.div`
     background-color: ${lightPrimary};
@@ -139,18 +144,20 @@ const CustomSelect = styled.select`
     cursor: pointer;
 `;
 
-interface HotelInfoProps {
-    name: string;
-    isLuxary: boolean;
-    isNormal: boolean;
-    isReallyBad: boolean;
-    city: string;
-    country: string;
-    place: string;
-    hotelFood: boolean;
-    hotelWiFi: boolean;
-    rate: number;
-    price: number;
+function filterHotels(
+    hotel: HotelInfoProps,
+    { city, food, wifi, lowPrice, highPrice }: Filters,
+): boolean {
+    const badConditions = [
+        city && city != hotel.city,
+        food && food != hotel.hotelFood,
+        wifi && wifi != hotel.hotelWiFi,
+        lowPrice && lowPrice > hotel.price,
+        highPrice && highPrice < hotel.price,
+    ];
+    const goodHotel = badConditions.every(condition => condition == false);
+    const badHotel = !goodHotel;
+    return !badHotel;
 }
 
 function SearchHotel() {
@@ -166,11 +173,8 @@ function SearchHotel() {
     const [departureDate, setDepartureDate] = useState("");
     const [peopleAmount, setPeopleAmount] = useState(0);
     const [city, setCity] = useState("");
-    const [lowPrice, setLowPrice] = useState(0);
-    const [highPrice, setHighPrice] = useState(0);
     const [parking, setParking] = useState(false);
-    const [food, setFood] = useState(false);
-    const [wifi, setWifi] = useState(false);
+
     const handleArrivalDateChange = (
         e: React.ChangeEvent<HTMLInputElement>,
     ) => {
@@ -187,167 +191,35 @@ function SearchHotel() {
         setPeopleAmount(Number(e.target.value));
     };
     const handleCityChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        setCity(e.target.value);
+        setUserFilters({ ...userFilters, city: e.target.value });
     };
     const handleLowPriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setLowPrice(Number(e.target.value));
+        setUserFilters({ ...userFilters, lowPrice: Number(e.target.value) });
     };
     const handleHighPriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setHighPrice(Number(e.target.value));
+        setUserFilters({ ...userFilters, highPrice: Number(e.target.value) });
     };
     const handleParkingChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setParking(e.target.checked);
     };
     const handleFoodChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setFood(e.target.checked);
+        setUserFilters({ ...userFilters, food: e.target.checked });
     };
     const handleWifiChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setWifi(e.target.checked);
+        setUserFilters({ ...userFilters, wifi: e.target.checked });
     };
-
-    function FilterHotels(hotel: HotelInfoProps): boolean {
-        let isCity = 0
-        let isFood = 0
-        let isWiFi = 0
-        let isLowPrice = 0
-        let isHighPrice = 0 
-        if (city != "") {
-            isCity = 1
-            if (city == hotel.city)
-                isCity = 2
-        }
-        if (food != false) {
-            isFood = 1
-            if (food == hotel.hotelFood)
-                isFood = 2
-        }
-        if (wifi != false) {
-            isWiFi = 1
-            if (wifi == hotel.hotelWiFi)
-                isWiFi = 2
-        }
-        if (lowPrice != 0) {
-            isLowPrice = 1
-            if (lowPrice < hotel.price)
-                isLowPrice = 2
-        }
-        if (highPrice != 0) {
-            isHighPrice = 1
-            if (highPrice > hotel.price)
-                isHighPrice = 2
-        }
-        const parameters = [isCity, isFood, isWiFi, isLowPrice, isHighPrice]
-        let isGood = true
-        for (let a of parameters)
-            if (a == 1)
-                {
-                    isGood = false
-                    break;
-                }
-        return isGood;
-    }
-
-    const hotelInfo = [
-        {
-            name: "Звезда",
-            isLuxary: false,
-            isNormal: true,
-            isReallyBad: false,
-            city: "Vladivostok",
-            country: "Russia",
-            place: "Svetlanskaya, bld. 118, appt. 51",
-            hotelFood: true,
-            hotelWiFi: true,
-            rate: 5,
-            price: 2600,
-        },
-
-        {
-            name: "Аврора",
-            isLuxary: true,
-            isNormal: true,
-            isReallyBad: false,
-            city: "Stavropol",
-            country: "Russia",
-            place: "Dzerzhinskogo Ul., bld. 139, appt. 12",
-            hotelFood: false,
-            hotelWiFi: false,
-            rate: 7,
-            price: 4000,
-        },
-
-        {
-            name: "IRIS",
-            isLuxary: true,
-            isNormal: false,
-            isReallyBad: true,
-            city: "Washington",
-            country: "USA",
-            place: "1144 Hardman Road",
-            hotelFood: false,
-            hotelWiFi: true,
-            rate: 6,
-            price: 2300,
-        },
-
-        {
-            name: "Galaxy",
-            isLuxary: false,
-            isNormal: true,
-            isReallyBad: false,
-            city: "Houston",
-            country: "USA",
-            place: "4275 Swick Hill Street",
-            hotelFood: true,
-            hotelWiFi: false,
-            rate: 8,
-            price: 3500,
-        },
-
-        {
-            name: "Мечта",
-            isLuxary: true,
-            isNormal: true,
-            isReallyBad: false,
-            city: "Saint-Petersburg",
-            country: "Russia",
-            place: "Antonova-Ovseenko Ul., bld. 5/21, appt. 179",
-            hotelFood: true,
-            hotelWiFi: true,
-            rate: 9,
-            price: 4500,
-        },
-        {
-            name: "Лагуна",
-            isLuxary: false,
-            isNormal: true,
-            isReallyBad: false,
-            city: "Stavropol",
-            country: "Russia",
-            place: "Prospekt Oktyabrya, bld. 48, appt. 35",
-            hotelFood: false,
-            hotelWiFi: true,
-            rate: 6,
-            price: 1700,
-        },
-        {
-            name: "Sky&Stars",
-            isLuxary: true,
-            isNormal: true,
-            isReallyBad: false,
-            city: "Houston",
-            country: "USA",
-            place: "3715 Oakwood Avenue",
-            hotelFood: true,
-            hotelWiFi: true,
-            rate: 8,
-            price: 5500,
-        },
-    ];
-    const cityNames = [...new Set( hotelInfo.map(one => one.city))];
+    const cityNames = [...new Set(hotelInfo.map(one => one.city))];
     const cities = cityNames.map(item => <option key={item}>{item}</option>);
-    const [hotelList, setHotelList] = useState(
-        hotelInfo.map(one => (
+    const [userFilters, setUserFilters] = useState({
+        city: "",
+        food: false,
+        wifi: false,
+        lowPrice: 0,
+        highPrice: 0,
+    });
+    const hotelList = hotelInfo
+        .filter(hotel => filterHotels(hotel, userFilters))
+        .map(one => (
             <HotelCard
                 name={one.name}
                 luxary={one.isLuxary}
@@ -365,8 +237,7 @@ function SearchHotel() {
                 raiting={one.rate}
                 price={one.price}
             />
-        )),
-    );
+        ));
     return (
         <>
             <Container>
@@ -428,33 +299,7 @@ function SearchHotel() {
                     <H2Filters>Питание</H2Filters>
                     <Checkbox type="checkbox" onChange={handleWifiChange} />
                     <H2Filters>Wi-Fi</H2Filters>
-                    <FindButton
-                        onClick={() => {
-                            setHotelList(
-                                hotelInfo
-                                    .filter(FilterHotels)
-                                    .map(one => (
-                                        <HotelCard
-                                            name={one.name}
-                                            luxary={one.isLuxary}
-                                            normal={one.isNormal}
-                                            reallyBad={one.isReallyBad}
-                                            address={
-                                                {
-                                                    city: one.city,
-                                                    country: one.country,
-                                                    place: one.place,
-                                                } as Address
-                                            }
-                                            isFood={one.hotelFood}
-                                            isWiFi={one.hotelWiFi}
-                                            raiting={one.rate}
-                                            price={one.price}
-                                        />
-                                    )),
-                            );
-                        }}
-                    >
+                    <FindButton>
                         <H2Headings>Найти</H2Headings>
                     </FindButton>
                 </ContainerDown>
