f88af9e717e38e199a36fab63a552a6b31e656c7
Salavat Nursafin <shattered946@gmail.com>
2024-06-02 09:35:00 +0400

    add dotenv

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,11 @@
+import "dotenv/config";
 import { app } from "./app";
 import { mongoURL, dbNameTest, port } from "./config";
 import MongoConnector from "./dbConnector";
 
-MongoConnector.connect(mongoURL, dbNameTest);
+const mongoHost = process.env.MONGO_HOST ?? "127.0.0.1";
+const mongoUrl = `mongodb://${mongoHost}:27017/`;
+MongoConnector.connect(mongoUrl, dbNameTest);
 app.listen(port, () => {
     console.log(
         "ехал гослинг навстречу концовке\n сценарист сказал не переживай\n гослинг ответил не переживу"
