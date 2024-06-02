3d7d9eb9744e024a0f906fd31ae338a4dbe015d8
varizonix <varizonix@gmail.com>
2024-06-02 15:17:39 +0400

    Фильтрация отелей

--- frontend/src/components/SearchHotel.tsx
+++ frontend/src/components/SearchHotel.tsx
@@ -14,6 +14,8 @@ import {
 import { useState } from "react";
 import HotelCard from "./HotelCard";
 import type { Address } from "samolet-common";
+import type { THotel } from "samolet-common/src/network/hotel";
+import { Photo } from "./HotelReviewComponent/style";
 
 const Container = styled.div`
     background-color: ${lightPrimary};
@@ -137,12 +139,23 @@ const CustomSelect = styled.select`
     cursor: pointer;
 `;
 
+interface HotelInfoProps {
+    name: string;
+    isLuxary: boolean;
+    isNormal: boolean;
+    isReallyBad: boolean;
+    city: string;
+    country: string;
+    place: string;
+    hotelFood: boolean;
+    hotelWiFi: boolean;
+    rate: number;
+    price: number;
+}
+
 function SearchHotel() {
-    const cityNames = ["one", "two", "three", "four", "five"];
-    const cities = cityNames.map(item => <option key={item}>{item}</option>);
     const guestAmount = [1, 2, 3, 4, 5, 6, 7, 8];
     const guests = guestAmount.map(item => <option key={item}>{item}</option>);
-
     const dispatch = useAppDispatch();
     const requestAllHotels = useAppSelector(
         state => state.requests.getallhotels,
@@ -192,21 +205,168 @@ function SearchHotel() {
         setWifi(e.target.checked);
     };
 
-    let hotelList = cityNames.map(one => (
-        <HotelCard
-            name={one}
-            luxary={true}
-            normal={false}
-            reallyBad={true}
-            address={
-                { city: "ddff ", country: "djjf ", place: "fjff" } as Address
-            }
-            isFood={true}
-            isWiFi={false}
-            raiting={7}
-            price={3200}
-        />
-    )); //works
+    function FilterHotels(hotel: HotelInfoProps): boolean {
+        let isCity = 0
+        let isFood = 0
+        let isWiFi = 0
+        let isLowPrice = 0
+        let isHighPrice = 0 
+        if (city != "") {
+            isCity = 1
+            if (city == hotel.city)
+                isCity = 2
+        }
+        if (food != false) {
+            isFood = 1
+            if (food == hotel.hotelFood)
+                isFood = 2
+        }
+        if (wifi != false) {
+            isWiFi = 1
+            if (wifi == hotel.hotelWiFi)
+                isWiFi = 2
+        }
+        if (lowPrice != 0) {
+            isLowPrice = 1
+            if (lowPrice < hotel.price)
+                isLowPrice = 2
+        }
+        if (highPrice != 0) {
+            isHighPrice = 1
+            if (highPrice > hotel.price)
+                isHighPrice = 2
+        }
+        const parameters = [isCity, isFood, isWiFi, isLowPrice, isHighPrice]
+        let isGood = true
+        for (let a of parameters)
+            if (a == 1)
+                {
+                    isGood = false
+                    break;
+                }
+        return isGood;
+    }
+
+    const hotelInfo = [
+        {
+            name: "Звезда",
+            isLuxary: false,
+            isNormal: true,
+            isReallyBad: false,
+            city: "Vladivostok",
+            country: "Russia",
+            place: "Svetlanskaya, bld. 118, appt. 51",
+            hotelFood: true,
+            hotelWiFi: true,
+            rate: 5,
+            price: 2600,
+        },
+
+        {
+            name: "Аврора",
+            isLuxary: true,
+            isNormal: true,
+            isReallyBad: false,
+            city: "Stavropol",
+            country: "Russia",
+            place: "Dzerzhinskogo Ul., bld. 139, appt. 12",
+            hotelFood: false,
+            hotelWiFi: false,
+            rate: 7,
+            price: 4000,
+        },
+
+        {
+            name: "IRIS",
+            isLuxary: true,
+            isNormal: false,
+            isReallyBad: true,
+            city: "Washington",
+            country: "USA",
+            place: "1144 Hardman Road",
+            hotelFood: false,
+            hotelWiFi: true,
+            rate: 6,
+            price: 2300,
+        },
+
+        {
+            name: "Galaxy",
+            isLuxary: false,
+            isNormal: true,
+            isReallyBad: false,
+            city: "Houston",
+            country: "USA",
+            place: "4275 Swick Hill Street",
+            hotelFood: true,
+            hotelWiFi: false,
+            rate: 8,
+            price: 3500,
+        },
+
+        {
+            name: "Мечта",
+            isLuxary: true,
+            isNormal: true,
+            isReallyBad: false,
+            city: "Saint-Petersburg",
+            country: "Russia",
+            place: "Antonova-Ovseenko Ul., bld. 5/21, appt. 179",
+            hotelFood: true,
+            hotelWiFi: true,
+            rate: 9,
+            price: 4500,
+        },
+        {
+            name: "Лагуна",
+            isLuxary: false,
+            isNormal: true,
+            isReallyBad: false,
+            city: "Stavropol",
+            country: "Russia",
+            place: "Prospekt Oktyabrya, bld. 48, appt. 35",
+            hotelFood: false,
+            hotelWiFi: true,
+            rate: 6,
+            price: 1700,
+        },
+        {
+            name: "Sky&Stars",
+            isLuxary: true,
+            isNormal: true,
+            isReallyBad: false,
+            city: "Houston",
+            country: "USA",
+            place: "3715 Oakwood Avenue",
+            hotelFood: true,
+            hotelWiFi: true,
+            rate: 8,
+            price: 5500,
+        },
+    ];
+    const cityNames = hotelInfo.map(one => one.city);
+    const cities = cityNames.map(item => <option key={item}>{item}</option>);
+    const [hotelList, setHotelList] = useState(
+        hotelInfo.map(one => (
+            <HotelCard
+                name={one.name}
+                luxary={one.isLuxary}
+                normal={one.isNormal}
+                reallyBad={one.isReallyBad}
+                address={
+                    {
+                        city: one.city,
+                        country: one.country,
+                        place: one.place,
+                    } as Address
+                }
+                isFood={one.hotelFood}
+                isWiFi={one.hotelWiFi}
+                raiting={one.rate}
+                price={one.price}
+            />
+        )),
+    );
     return (
         <>
             <Container>
@@ -270,8 +430,29 @@ function SearchHotel() {
                     <H2Filters>Wi-Fi</H2Filters>
                     <FindButton
                         onClick={() => {
-                            dispatch(getAllHotelsThunk());
-                            alert(hotels?.length);
+                            setHotelList(
+                                hotelInfo
+                                    .filter(FilterHotels)
+                                    .map(one => (
+                                        <HotelCard
+                                            name={one.name}
+                                            luxary={one.isLuxary}
+                                            normal={one.isNormal}
+                                            reallyBad={one.isReallyBad}
+                                            address={
+                                                {
+                                                    city: one.city,
+                                                    country: one.country,
+                                                    place: one.place,
+                                                } as Address
+                                            }
+                                            isFood={one.hotelFood}
+                                            isWiFi={one.hotelWiFi}
+                                            raiting={one.rate}
+                                            price={one.price}
+                                        />
+                                    )),
+                            );
                         }}
                     >
                         <H2Headings>Найти</H2Headings>
