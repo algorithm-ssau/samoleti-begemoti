db1fcfd27087447d13761528d4e9693fea83b31a
Salavat Nursafin <shattered946@gmail.com>
2024-06-06 22:03:17 +0400

    restrict access to hotel creation

--- backend/src/routers/HotelsRouter.ts
+++ backend/src/routers/HotelsRouter.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import { HotelController } from "../controllers/index";
+import { adminMiddleware } from "../middleware/AdminAuthMiddleware";
 
-export const hotelRouter = express.Router();
-
-hotelRouter.get("/search/hotel", HotelController.getByPlace);
-hotelRouter.get("/hotels", HotelController.getAll);
-hotelRouter.get("/hotels/:id", HotelController.getOne);
-hotelRouter.post("/hotels-full", HotelController.createFull);
-hotelRouter.post("/hotels", HotelController.create);
-hotelRouter.delete("/hotels/:id", HotelController.deleteOne);
-hotelRouter.patch("/hotels/:id", HotelController.updateOne);
+export const hotelRouter = express
+    .Router()
+    .get("/search/hotel", HotelController.getByPlace)
+    .get("/hotels", HotelController.getAll)
+    .get("/hotels/:id", HotelController.getOne)
+    .use(adminMiddleware)
+    .post("/hotels", HotelController.create)
+    .delete("/hotels/:id", HotelController.deleteOne)
+    .patch("/hotels/:id", HotelController.updateOne)
+    .post("/hotels-full", HotelController.createFull);
